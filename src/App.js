4dd3c2356443ae5a3af9d4733954028d4f1a7712
saif eddine romdhane <32793306+saifEddineR@users.noreply.github.com>
2020-12-14 22:07:50 +0100

    reactState-checkpoint

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
+import React, { Component } from 'react'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
+
+export default class App extends Component {
+  state = {
+    hidden : false,
+    fullName: 'Elon Musk',
+    bio: 'South African entrepreneur Elon Musk is known for founding Tesla Motors and SpaceX, which launched a landmark commercial spacecraft in 2012.',
+    imgSrc:'https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cg_face%2Cq_auto:good%2Cw_300/MTY2MzU3Nzk2OTM2MjMwNTkx/elon_musk_royal_society.jpg',
+    profession: 'CEO of Tesla Motors, SpaceX ..',
+    counter:0
+  };
+  toggleBtn = () => {
+    this.setState({hidden : !this.state.hidden});
+  }
+  componentDidMount() {
+    setInterval((el) => {
+      el = this.state.counter + 1;
+      this.state.counter = el;
+      this.render()
+      console.log(el) //it's counting in the console but not updating! cause there is no re-render
+    }, 1000)
+  }
+  render() {
+    return (
+      <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={this.toggleBtn} >Click Here!</button>
+        <p className="counter" >counter : {this.state.counter} </p>
+        <article>{ this.state.hidden ? 
+        <div><h3>{this.state.fullName}</h3>
+        <p>{this.state.bio}</p> 
+        <img src ={this.state.imgSrc}/> 
+        <p>{this.state.profession} </p></div> :
+        <p>click the button to show content</p> }</article>
+          
       </header>
     </div>
-  );
+    )
+  }
+
+  
 }
 
-export default App;
+
